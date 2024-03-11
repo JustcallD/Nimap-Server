5e1d5a3a470ffde999b739fc5698f27f0f12d128
JustcallD <mahajandhiru001@gmail.com>
2024-03-11 19:43:48 +0530

    improve server

--- Routes/Category.js
+++ Routes/Category.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Category = require("../Schema/category");
+const Product = require("../Schema/product");
 
 // GET all categories
 router.get("/", async (req, res) => {
@@ -25,18 +26,21 @@ router.post("/", async (req, res) => {
   }
 });
 
-// GET a single category
-router.get("/:id", getCategory, (req, res) => {
-  res.json(res.category);
-});
-
 // UPDATE a category
-router.patch("/:id", getCategory, async (req, res) => {
-  if (req.body.name != null) {
-    res.category.name = req.body.name;
-  }
+router.patch("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
   try {
-    const updatedCategory = await res.category.save();
+    const updatedCategory = await Category.findByIdAndUpdate(
+      id,
+      {
+        $set: {
+          name,
+        },
+      },
+      { new: true }
+    );
     res.json(updatedCategory);
   } catch (error) {
     res.status(400).json({ message: error.message });
@@ -44,27 +48,33 @@ router.patch("/:id", getCategory, async (req, res) => {
 });
 
 // DELETE a category
-router.delete("/:id", getCategory, async (req, res) => {
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
   try {
-    await res.category.remove();
+    const defaultCategoryId = await Category.findOne({ name: "Default" });
+
+    if (!defaultCategoryId) {
+      throw new Error("Default category not found");
+    }
+
+    if (defaultCategoryId._id.toString() === id) {
+      throw new Error("Cannot delete the default category");
+    }
+    await Category.findByIdAndDelete(id);
+    const productsToUpdate = await Product.find({ categoryId: id });
+    const defaultCategory = await Category.findOne({ name: "Default" });
+
+    await Promise.all(
+      productsToUpdate.map(async (product) => {
+        product.categoryId = defaultCategory._id;
+        await product.save();
+      })
+    );
+
     res.json({ message: "Category deleted" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
-
-async function getCategory(req, res, next) {
-  let category;
-  try {
-    category = await Category.findById(req.params.id);
-    if (category == null) {
-      return res.status(404).json({ message: "Category not found" });
-    }
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
-  }
-  res.category = category;
-  next();
-}
 
 module.exports = router;
--- Routes/Product.js
+++ Routes/Product.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Product = require("../Schema/product");
+const category = require("../Schema/category");
 
 // GET products with pagination
 router.get("/", async (req, res) => {
@@ -32,5 +33,42 @@ router.post("/", async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 });
+router.patch("/:id", async (req, res) => {
+  const id = req.params.id;
+  const { name, categoryId } = req.body;
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      id,
+      {
+        $set: {
+          name,
+          categoryId,
+        },
+      },
+      {
+        new: true,
+      }
+    );
+    if (!updatedProduct) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.json(updatedProduct);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
+// DELETE a product
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const deletedProduct = await Product.findByIdAndDelete(id);
+    if (!deletedProduct) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.json({ message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 module.exports = router;

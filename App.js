34dec419e9993b40e03d9e034f0dedc2cc93560f
JustcallD <mahajandhiru001@gmail.com>
2024-03-11 20:07:35 +0530

    some update

--- App.js
+++ App.js
@@ -4,18 +4,21 @@ const cors = require("cors");
 const app = express();
 
 // Apply middleware
-app.use(cors({ origin: "https://nimap-task.netlify.app", credentials: true }));
+app.use(
+  cors({ origin: ["*", "https://nimap-task.netlify.app"], credentials: true })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // routes imports
-const categoriesRouter = require('./Routes/Category');
-const productsRouter = require('./Routes/Product');
+const categoriesRouter = require("./Routes/Category");
+const productsRouter = require("./Routes/Product");
 
-app.use('/categories', categoriesRouter);
-app.use('/products', productsRouter);
+app.use("/categories", categoriesRouter);
+app.use("/products", productsRouter);
 app.get("/", (req, res) => {
   res.send("server running");
 });
 
 module.exports = { app };
+// https://nimap-task.netlify.app

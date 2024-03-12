0d8e89432a5798f124a7da974e93c6fc8a86c09e
JustcallD <mahajandhiru001@gmail.com>
2024-03-12 12:14:45 +0530

    some changes

--- index.js
+++ index.js
@@ -8,16 +8,3 @@ app.listen(process.env.PORT || 4002, () => {
   );
 });
 
-// var conn = mysql.createConnection({
-//   host: "nimapsql.mysql.database.azure.com",
-//   user: "justcalld",
-//   password: "{your_password}",
-//   database: "{your_database}",
-//   port: 3306,
-//   ssl: { ca: fs.readFileSync("{ca-cert filename}") },
-// });
-
-// host: 'sql.freedb.tech',
-// user: 'freedb_adminmohit',
-// password: 'B!c8tYCpW@yxSW3',
-// database: 'freedb_nimaptask'

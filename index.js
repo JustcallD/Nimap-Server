6956fb1c330e498873d58ca74d6d1d74f412cb21
JustcallD <mahajandhiru001@gmail.com>
2024-03-12 15:54:33 +0530

    test

--- index.js
+++ index.js
@@ -8,11 +8,3 @@ app.listen(process.env.PORT || 4002, () => {
   );
 });
 
-// var conn = mysql.createConnection({
-//   host: "nimapsql.mysql.database.azure.com",
-//   user: "justcalld",
-//   password: "{your_password}",
-//   database: "{your_database}",
-//   port: 3306,
-//   ssl: { ca: fs.readFileSync("{ca-cert filename}") },
-// });
